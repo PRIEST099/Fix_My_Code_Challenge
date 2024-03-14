99a0417994c554b28d2d70788e3b8b3dca064946
Ahadi <ahadic044@gmail.com>
2024-03-14 18:15:12 +0200

    refining the whole square file

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-""" a python script to define square class"""
+""" Module for square class"""
 
 
-class square():
-    """a square dimensions class"""
-
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """initialiser for the class"""
-
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """perimeter of a square"""
-
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """string representation of a square"""
-
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """Create a square class object"""
-    s = square(width=12, height=9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
